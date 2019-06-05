9839c64ac0f130bb775e13279effc6e345867251
Fsantos123 <44686168+Fsantos123@users.noreply.github.com>
2019-06-05 19:51:28 -0300

    adicionei o arquivo Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+* Hello World Java
+*@author Santos
+*/
+
+public class Hello{
+		//Método principal
+		public static void main(String[] args){
+			System.out.println("Hello World");
+		}
+	
+	
+	
+}
